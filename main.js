9b88d46ba6fad81024aa6cbcf112b2776caa3342
TrongLam1 <135250023+TrongLam1@users.noreply.github.com>
2023-08-16 01:57:11 +0700

    update

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 window.addEventListener('scroll', function () {
-    var utilityHeader = document.querySelector('.utilityHeaderWrap');
     var headerPrimary = document.querySelector('.headerPrimaryWrap');
     var scrollPosition = window.scrollY;
 
@@ -8,4 +7,55 @@ window.addEventListener('scroll', function () {
     } else {
         headerPrimary.style.top = '50px';
     }
+});
+
+// Modal drop down
+const modalHeader = document.querySelector('.modalHeader');
+const dropDownItems = modalHeader.getElementsByClassName('headerDropDownItem');
+const primaryDrops = document.getElementsByClassName('headerPrimaryDrop');
+const headerWrap = document.querySelector('.wrap-header');
+const dropSearch = document.querySelector('.dropDownSearch');
+const body = document.body;
+
+Array.from(primaryDrops).forEach((element, index) => {
+    element.addEventListener('click', () => {
+        modalHeader.classList.add('active');
+        headerWrap.classList.add('active');
+        body.style.overflow = 'hidden';
+
+        for (let i = 0; i < dropDownItems.length; i++) {
+            if (i === index) {
+                dropDownItems[i].classList.add('active');
+            } else {
+                dropDownItems[i].classList.remove('active');
+            }
+        }
+    });
+});
+
+dropSearch.addEventListener('click', () => {
+    modalHeader.classList.add('active');
+    headerWrap.classList.add('active');
+    body.style.overflow = 'hidden';
+});
+
+modalHeader.addEventListener('click', () => {
+    modalHeader.classList.remove('active');
+    headerWrap.classList.remove('active');
+    body.style.overflow = 'auto';
+});
+
+// Modal sign in
+const signInModal = document.querySelector('.modalSignIn');
+const signInBtn = document.querySelector('.headerSignIn');
+const closeBtn = document.querySelector('.closeSignIn');
+
+signInBtn.addEventListener('click', () => {
+    signInModal.classList.add('active');
+    body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+    signInModal.classList.remove('active');
+    body.style.overflow = 'auto';
 });
